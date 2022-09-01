b4fc078fb26084c4f9c8908dfea26359b4906c56
Lila Atmani <sunbornone@gmail.com>
2022-09-01 12:30:43 +0200

    resultats quizz determinés en js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const form = document.querySelector('.form-quizz');
 let tableauResultats = [];
+const reponses = ['b','c','b','a','c']; 
+const emojis = ['✔️','✨','👀','😭','👎'];
+const titreResultat = document.querySelector('.resultats h2');
+const texteResultat = document.querySelector('.note');
+const aideResultat = document.querySelector('.aide');
+const toutesLesQuestions = document.querySelectorAll('.question-block');
+let verifTableau = []; 
+
 
 form.addEventListener('submit', (e) => {
 e.preventDefault();
@@ -10,5 +18,18 @@ for(i = 1; i < 6; i++) {
     tableauResultats.push(document.querySelector(`input[name="q${i}"]:checked`).value)
 }
 console.log(tableauResultats);
+verifFunc(tableauResultats);
 tableauResultats = [];
 })
+
+function verifFunc(tabResultats) {
+    for(let a = 0; a < 5; a++){
+        if(tabResultats[a] === reponses[a]) {
+            verifTableau.push(true);
+        } else {
+            verifTableau.push(false);
+        }
+}
+console.log(verifTableau);
+verifTableau = [];
+}
